8ddfc21b1cec3ba20fef80130353eabcd2c4b415
Stephen Edurado Camilo <stephen.camilo@live.com>
2021-02-03 23:53:57 -0300

    Start again

--- core/modules/field_ui/field_ui.api.php
+++ core/modules/field_ui/field_ui.api.php
@@ -11,195 +11,115 @@
  */
 
 /**
- * Add settings to a field settings form.
+ * Allow modules to add settings to field formatters provided by other modules.
  *
- * Invoked from field_ui_field_settings_form() to allow the module defining the
- * field to add global settings (i.e. settings that do not depend on the bundle
- * or instance) to the field settings form. If the field already has data, only
- * include settings that are safe to change.
+ * @param \Drupal\Core\Field\FormatterInterface $plugin
+ *   The instantiated field formatter plugin.
+ * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+ *   The field definition.
+ * @param string $view_mode
+ *   The entity view mode.
+ * @param array $form
+ *   The (entire) configuration form array.
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ *   The form state.
  *
- * @todo: Only the field type module knows which settings will affect the
- * field's schema, but only the field storage module knows what schema
- * changes are permitted once a field already has data. Probably we need an
- * easy way for a field type module to ask whether an update to a new schema
- * will be allowed without having to build up a fake $prior_field structure
- * for hook_field_update_forbid().
+ * @return array
+ *   Returns the form array to be built.
  *
- * @param $field
- *   The field structure being configured.
- * @param $instance
- *   The instance structure being configured.
- * @param $has_data
- *   TRUE if the field already has data, FALSE if not.
- *
- * @return
- *   The form definition for the field settings.
+ * @see \Drupal\field_ui\Form\EntityViewDisplayEditForm::thirdPartySettingsForm()
  */
-function hook_field_settings_form($field, $instance, $has_data) {
-  $settings = $field['settings'];
-  $form['max_length'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Maximum length'),
-    '#default_value' => $settings['max_length'],
-    '#required' => FALSE,
-    '#element_validate' => array('element_validate_integer_positive'),
-    '#description' => t('The maximum length of the field in characters. Leave blank for an unlimited size.'),
-  );
-  return $form;
-}
-
-/**
- * Add settings to an instance field settings form.
- *
- * Invoked from field_ui_field_edit_form() to allow the module defining the
- * field to add settings for a field instance.
- *
- * @param $field
- *   The field structure being configured.
- * @param $instance
- *   The instance structure being configured.
- *
- * @return
- *   The form definition for the field instance settings.
- */
-function hook_field_instance_settings_form($field, $instance) {
-  $settings = $instance['settings'];
-
-  $form['text_processing'] = array(
-    '#type' => 'radios',
-    '#title' => t('Text processing'),
-    '#default_value' => $settings['text_processing'],
-    '#options' => array(
-      t('Plain text'),
-      t('Filtered text (user selects text format)'),
-    ),
-  );
-  if ($field['type'] == 'text_with_summary') {
-    $form['display_summary'] = array(
-      '#type' => 'select',
-      '#title' => t('Display summary'),
-      '#options' => array(
-        t('No'),
-        t('Yes'),
-      ),
-      '#description' => t('Display the summary to allow the user to input a summary value. Hide the summary to automatically fill it with a trimmed portion from the main post.'),
-      '#default_value' => !empty($settings['display_summary']) ? $settings['display_summary'] :  0,
-    );
-  }
-
-  return $form;
-}
-
-/**
- * Add settings to a widget settings form.
- *
- * Invoked from field_ui_field_edit_form() to allow the module defining the
- * widget to add settings for a widget instance.
- *
- * @param $field
- *   The field structure being configured.
- * @param $instance
- *   The instance structure being configured.
- *
- * @return
- *   The form definition for the widget settings.
- */
-function hook_field_widget_settings_form($field, $instance) {
-  $widget = $instance['widget'];
-  $settings = $widget['settings'];
-
-  if ($widget['type'] == 'text_textfield') {
-    $form['size'] = array(
-      '#type' => 'textfield',
-      '#title' => t('Size of textfield'),
-      '#default_value' => $settings['size'],
-      '#element_validate' => array('element_validate_integer_positive'),
-      '#required' => TRUE,
-    );
-  }
-  else {
-    $form['rows'] = array(
-      '#type' => 'textfield',
-      '#title' => t('Rows'),
-      '#default_value' => $settings['rows'],
-      '#element_validate' => array('element_validate_integer_positive'),
-      '#required' => TRUE,
-    );
-  }
-
-  return $form;
-}
-
-
-/**
- * Specify the form elements for a formatter's settings.
- *
- * This hook is only invoked if hook_field_formatter_settings_summary()
- * returns a non-empty value.
- *
- * @param $field
- *   The field structure being configured.
- * @param $instance
- *   The instance structure being configured.
- * @param $view_mode
- *   The view mode being configured.
- * @param $form
- *   The (entire) configuration form array, which will usually have no use here.
- * @param $form_state
- *   The form state of the (entire) configuration form.
- *
- * @return
- *   The form elements for the formatter settings.
- */
-function hook_field_formatter_settings_form($field, $instance, $view_mode, $form, &$form_state) {
-  $display = $instance['display'][$view_mode];
-  $settings = $display['settings'];
-
+function hook_field_formatter_third_party_settings_form(\Drupal\Core\Field\FormatterInterface $plugin, \Drupal\Core\Field\FieldDefinitionInterface $field_definition, $view_mode, array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
   $element = [];
-
-  if ($display['type'] == 'text_trimmed' || $display['type'] == 'text_summary_or_trimmed') {
-    $element['trim_length'] = array(
-      '#title' => t('Length'),
-      '#type' => 'textfield',
-      '#size' => 20,
-      '#default_value' => $settings['trim_length'],
-      '#element_validate' => array('element_validate_integer_positive'),
-      '#required' => TRUE,
-    );
+  // Add a 'my_setting' checkbox to the settings form for 'foo_formatter' field
+  // formatters.
+  if ($plugin->getPluginId() == 'foo_formatter') {
+    $element['my_setting'] = [
+      '#type' => 'checkbox',
+      '#title' => t('My setting'),
+      '#default_value' => $plugin->getThirdPartySetting('my_module', 'my_setting'),
+    ];
   }
-
   return $element;
-
 }
 
 /**
- * Return a short summary for the current formatter settings of an instance.
+ * Allow modules to add settings to field widgets provided by other modules.
  *
- * If an empty result is returned, the formatter is assumed to have no
- * configurable settings, and no UI will be provided to display a settings
- * form.
+ * @param \Drupal\Core\Field\WidgetInterface $plugin
+ *   The instantiated field widget plugin.
+ * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+ *   The field definition.
+ * @param string $form_mode
+ *   The entity form mode.
+ * @param array $form
+ *   The (entire) configuration form array.
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ *   The form state.
  *
- * @param $field
- *   The field structure.
- * @param $instance
- *   The instance structure.
- * @param $view_mode
- *   The view mode for which a settings summary is requested.
+ * @return array
+ *   Returns the form array to be built.
  *
- * @return
- *   A string containing a short summary of the formatter settings.
+ * @see \Drupal\field_ui\Form\EntityFormDisplayEditForm::thirdPartySettingsForm()
  */
-function hook_field_formatter_settings_summary($field, $instance, $view_mode) {
-  $display = $instance['display'][$view_mode];
-  $settings = $display['settings'];
-
-  $summary = '';
-
-  if ($display['type'] == 'text_trimmed' || $display['type'] == 'text_summary_or_trimmed') {
-    $summary = t('Length: @chars chars', array('@chars' => $settings['trim_length']));
+function hook_field_widget_third_party_settings_form(\Drupal\Core\Field\WidgetInterface $plugin, \Drupal\Core\Field\FieldDefinitionInterface $field_definition, $form_mode, array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  $element = [];
+  // Add a 'my_setting' checkbox to the settings form for 'foo_widget' field
+  // widgets.
+  if ($plugin->getPluginId() == 'foo_widget') {
+    $element['my_setting'] = [
+      '#type' => 'checkbox',
+      '#title' => t('My setting'),
+      '#default_value' => $plugin->getThirdPartySetting('my_module', 'my_setting'),
+    ];
   }
+  return $element;
+}
 
-  return $summary;
+/**
+ * Alters the field formatter settings summary.
+ *
+ * @param array $summary
+ *   An array of summary messages.
+ * @param array $context
+ *   An associative array with the following elements:
+ *   - formatter: The formatter plugin.
+ *   - field_definition: The field definition.
+ *   - view_mode: The view mode being configured.
+ *
+ * @see \Drupal\field_ui\Form\EntityViewDisplayEditForm::alterSettingsSummary()
+ */
+function hook_field_formatter_settings_summary_alter(array &$summary, array $context) {
+  // Append a message to the summary when an instance of foo_formatter has
+  // my_setting set to TRUE for the current view mode.
+  if ($context['formatter']->getPluginId() == 'foo_formatter') {
+    if ($context['formatter']->getThirdPartySetting('my_module', 'my_setting')) {
+      $summary[] = t('My setting enabled.');
+    }
+  }
+}
+
+/**
+ * Alters the field widget settings summary.
+ *
+ * @param array $summary
+ *   An array of summary messages.
+ * @param array $context
+ *   An associative array with the following elements:
+ *   - widget: The widget object.
+ *   - field_definition: The field definition.
+ *   - form_mode: The form mode being configured.
+ *
+ * @see \Drupal\field_ui\Form\EntityFormDisplayEditForm::alterSettingsSummary()
+ */
+function hook_field_widget_settings_summary_alter(array &$summary, array $context) {
+  // Append a message to the summary when an instance of foo_widget has
+  // my_setting set to TRUE for the current view mode.
+  if ($context['widget']->getPluginId() == 'foo_widget') {
+    if ($context['widget']->getThirdPartySetting('my_module', 'my_setting')) {
+      $summary[] = t('My setting enabled.');
+    }
+  }
 }
 
 /**
--- core/modules/statistics/statistics.php
+++ core/modules/statistics/statistics.php
@@ -2,32 +2,29 @@
 
 /**
  * @file
- * Handles counts of node views via Ajax with minimal bootstrap.
+ * Handles counts of node views via AJAX with minimal bootstrap.
  */
 
-/**
-* Root directory of Drupal installation.
-*/
-define('DRUPAL_ROOT', substr($_SERVER['SCRIPT_FILENAME'], 0, strpos($_SERVER['SCRIPT_FILENAME'], '/modules/statistics/statistics.php')));
-// Change the directory to the Drupal root.
-chdir(DRUPAL_ROOT);
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
 
-include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_VARIABLES);
-if (variable_get('statistics_count_content_views', 0) && $bootstrap->variable_get('statistics_count_content_views_ajax', 0)) {
-  if (isset($_POST['nid'])) {
-    $nid = $_POST['nid'];
-    if (is_numeric($nid)) {
-      db_merge('node_counter')
-        ->key(array('nid' => $nid))
-        ->fields(array(
-          'daycount' => 1,
-          'totalcount' => 1,
-          'timestamp' => REQUEST_TIME,
-        ))
-        ->expression('daycount', 'daycount + 1')
-        ->expression('totalcount', 'totalcount + 1')
-        ->execute();
-    }
+chdir('../../..');
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = DrupalKernel::createFromRequest(Request::createFromGlobals(), $autoloader, 'prod');
+$kernel->boot();
+$container = $kernel->getContainer();
+
+$views = $container
+  ->get('config.factory')
+  ->get('statistics.settings')
+  ->get('count_content_views');
+
+if ($views) {
+  $nid = filter_input(INPUT_POST, 'nid', FILTER_VALIDATE_INT);
+  if ($nid) {
+    $container->get('request_stack')->push(Request::createFromGlobals());
+    $container->get('statistics.storage.node')->recordView($nid);
   }
 }
